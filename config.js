4e803c2f5e62e7667ccf49b8720bb8d615efc8d5
Moussatech127 <tvvtez17@gmail.com>
2024-11-17 21:57:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -20,10 +20,10 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vakp0UnIC
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/vRR63CN.jpeg,https://i.imgur.com/ovCfp9J.jpeg,https://i.imgur.com/edzo5dj.jpeg";
 global.devs = "https://wa.me/50931461936 , https://wa.me/50931461936";
 global.sudo = process.env.SUDO || "50931461936";
-global.owner = process.env.OWNER_NUMBER || "PUT YOUR OWNER NUMBER";
+global.owner = process.env.OWNER_NUMBER || "221771084463";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "true";
+global.wlcm = process.env.WELCOME || "false";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
@@ -43,7 +43,7 @@ global.scan = "https://drk-tech-2.onrender.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "mettre votre session ici"
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FEbU5PNDV3Y0J4cUphdFNnbUh4ak5qZWt3SG5CdFBSOEtXUi91UWhWRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicHQ0dnMvaGcwWnZyU0FHZFNQd2VFNDUrN1dCaUF6d0U3QkVGZkM2bmpoYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2QVQ3djFDNzlxb1NWMW8vMnBubk5nWVNTTUtOVGE0ZWYvTWNPckNPZzIwPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJTc1hHRllOWSs3b3R1UHZDM2VpV1ZIbG5UYmhic1NmY3JvMzF1T0hhVkZFPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJMOHBTdlBCV0VvWWtFb2thS2hrUkNOVkErQWQ1UkZhbExOUDJ1cVVTbDA9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InY2VkdIci9XcGxEQjJkWkRHc1I4TzdveGEzZkFTcmthM2pueStJY2RDeDA9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibUMvLzZnaUFIakR5OHE2bVpJODI5b2cwSEN1MlhDL1R3a3RWeHhMUEVVUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUGhja0NWRVNkajM0SU9aUk11WTZ2Zk40QTMvbi9xUDhFMXcrQWtyc0xVUT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ildqa1FtYTg4U2dsVkRmZk9xYmtZZUlqeVdCRXpuc01RdXVQSGJHajhhN1I1N29uU3hxWlY1U0lGa2FmL2czYWx2d24vMnhndWlrc1J5QnBRRUZ6MWh3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NjQsImFkdlNlY3JldEtleSI6Ik9ZYnQ1TWdrKzk5YituOUdEY2pBSVlWMHZmcXpKdUhIejZmc0JpbkM2djA9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImJtUlRQQks3U0YtZDVwd3RjMWl4YlEiLCJwaG9uZUlkIjoiYzU0ZWZjZTgtNTZjNS00OTJjLThlNzctNDY5OWVlZTMyY2E4IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InFQc2F4TnJiRWVLcUxOa2pSZlJXOFZQNVF3bz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJHaGRrdUJIVlp1RCt0ZUtzSXM5MkQrY0k0aVE9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiNVg5MUFOUFEiLCJtZSI6eyJpZCI6IjIyMTc3MTA4NDQ2Mzo4NkBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLgvJLwnZSS8J2Un/CdlKbwnZSx8J2UrPCWp7fig53ig53wn4yf8J2Uq/CdlKLwnZS18J2UsfCdlJ7wnZSk8J2UouC8kiJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTzdNdTlZRUVOZko2YmtHR0EwZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5Ijoidm52akx2RVMzUlNYdk43UFM3RkxyOVIvSTZieHBGVXVSN2pPb2dBVmxsZz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiTXhaREVpcVlwZVVlOFJVRTF2dS9uNVdpTnAvOExJak5TVmVuZElKUXlHbjBPMWlzcVNuZmF5RXh0R3hRVnlVbXdoNXdNSE9OZVM0RG12V2tidUF3Q2c9PSIsImRldmljZVNpZ25hdHVyZSI6Im9EQmxRRXdXNVV0Nyt0YVdkQkYwRU1FRStBKzBud2ZnOGFWM2kzWUZtWGw2YjlockNJMy9JeXpnbVl4QktUUkllV3c3V25XQXVLL3JYL20xY3cxMGh3PT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjIxNzcxMDg0NDYzOjg2QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmI1NzR5N3hFdDBVbDd6ZXowdXhTNi9VZnlPbThhUlZMa2U0enFJQUZaWlkifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MzE4ODAxNjQsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBRWl3In0="
 module.exports = {
   menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || ".",
@@ -53,7 +53,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || " 𝙎-𝙏𝞢𝞜",
   packname: process.env.PACK_NAME || "𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨",
   botname: process.env.BOT_NAME || "ST┘𝑫𝑹𝑲-𝑹𝑨𝑮𝑵𝑨𖤐",
-  ownername: process.env.OWNER_NAME || "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑",
+  ownername: process.env.OWNER_NAME || "༒𝔒𝔟𝔦𝔱𝔬𖧷⃝⃝🌟𝔫𝔢𝔵𝔱𝔞𝔤𝔢༒",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "tCxobE1FqJgfbDZgsYQNHBFB",
